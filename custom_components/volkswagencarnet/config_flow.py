e53b4e08f20d5cff5ce67e53a2e3b41f00c4849e
Michael Wikberg <michael@wikberg.fi>
2022-02-07 13:25:27 +0200

    Update imports to work with volkswagencarnet package fixes

--- custom_components/volkswagencarnet/config_flow.py
+++ custom_components/volkswagencarnet/config_flow.py
@@ -13,7 +13,7 @@ from homeassistant.const import (
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from vw_connection import Connection
+from volkswagencarnet.vw_connection import Connection
 from . import get_convert_conf
 from .const import (
     CONF_CONVERT,
