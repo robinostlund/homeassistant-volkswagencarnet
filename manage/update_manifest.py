7d9b12371bbddd80bbff23a3f43a8116d7e74901
Robin Östlund <robin.ostlund@opsio.se>
2022-02-07 08:41:17 +0100

    making update_manifest.py set requirements from requirements.txt

--- manage/update_manifest.py
+++ manage/update_manifest.py
@@ -11,11 +11,21 @@ def update_manifest():
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
+    # read manifest
     with open(f"{os.getcwd()}/custom_components/volkswagencarnet/manifest.json") as manifestfile:
         manifest = json.load(manifestfile)
 
+    # set version in manifest
     manifest["version"] = version
 
+    # read requirements.txt
+    with open(f"{os.getcwd()}/requirements.txt") as requirementsfile:
+        requirements = [line.rstrip() for line in requirementsfile]
+
+    # set requirements in manifest
+    manifest["requirements"] = requirements
+
+    # save manifest
     with open(
         f"{os.getcwd()}/custom_components/volkswagencarnet/manifest.json", "w"
     ) as manifestfile:
