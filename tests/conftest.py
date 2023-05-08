8045f313ad5cc60b0c106c9dec6e942a0647436a
Robin Östlund <robin.ostlund@opsio.se>
2023-05-08 13:53:18 +0200

    fix some minor things for automatic tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,9 @@ def auto_enable_custom_integrations(enable_custom_integrations):
 @pytest.fixture
 def bypass_setup_fixture():
     """Prevent setup."""
-    with patch("custom_components.volkswagencarnet.async_setup", return_value=True,), patch(
+    with patch("custom_components.volkswagencarnet.async_setup",
+        return_value=True,
+    ), patch(
         "custom_components.volkswagencarnet.async_setup_entry",
         return_value=True,
     ):
