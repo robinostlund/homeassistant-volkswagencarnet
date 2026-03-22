f6fb2bf62201c449bf960983b8d2c96947424343
virtualdj <virtualdj@users.noreply.github.com>
2026-03-22 15:24:11 +0100

    Correct statistic history for last_trip_total entities using last_reset

--- custom_components/volkswagencarnet/__init__.py
+++ custom_components/volkswagencarnet/__init__.py
@@ -334,6 +334,14 @@ class VolkswagenEntity(CoordinatorEntity, RestoreEntity):
 
         self.instrument.callback = update_callbacks
 
+        # Set extra attributes for last trip sensors to persist start_km
+        try:
+            if self.instrument.attr.startswith("last_trip_total_"):
+                self._prev_km: int = 0
+                self._attr_extra_state_attributes = {}
+        except ValueError:
+            pass
+
     @callback
     def async_write_ha_state(self) -> None:
         """Write state to HA, but only if needed."""
@@ -347,6 +355,37 @@ class VolkswagenEntity(CoordinatorEntity, RestoreEntity):
             )
             super().async_write_ha_state()
             return
+        # Check starting km for last trip sensors
+        if (
+            self.instrument.attr.startswith("last_trip_total_")
+            and self.instrument.vehicle.is_last_trip_start_km_supported
+            and (current_start_km := self.instrument.vehicle.last_trip_start_km)
+            is not None
+        ):
+            # Load previous value from restore state if available
+            if (
+                self._prev_km == 0
+                and self.restored_state
+                and "start_km" in self.restored_state.attributes
+            ):
+                self._prev_km = int(self.restored_state.attributes["start_km"])
+
+            # Check if a new trip has started
+            if current_start_km > self._prev_km:
+                _LOGGER.debug(
+                    "%s: Detected new last trip with start km %s (previous was: %s)",
+                    self.name,
+                    current_start_km,
+                    self._prev_km,
+                )
+
+                # Update the last_reset attribute to sum the energy of the new trip
+                self._attr_last_reset = self.instrument.last_refresh
+
+            # Save current start km
+            self._prev_km = current_start_km
+            self._attr_extra_state_attributes["start_km"] = current_start_km
+
         # Get the previous state from the state machine if found
         prev: State | None = self.hass.states.get(self.entity_id)
 
@@ -382,6 +421,7 @@ class VolkswagenEntity(CoordinatorEntity, RestoreEntity):
             "last_updated",
             "assumed_state",
             "attribution",
+            "last_reset",
         }
         current_attrs = self.extra_state_attributes or {}
         prev_attrs = {k: v for k, v in prev.attributes.items() if k not in system_attrs}
@@ -499,6 +539,13 @@ class VolkswagenEntity(CoordinatorEntity, RestoreEntity):
 
         if self.vehicle.is_model_image_supported:
             attributes["image_url"] = self.vehicle.model_image
+
+        # Include extra attributes to be persisted
+        if (
+            hasattr(self, "_attr_extra_state_attributes")
+            and self._attr_extra_state_attributes
+        ):
+            attributes.update(self._attr_extra_state_attributes)
 
         return attributes
 
